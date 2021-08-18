2ff59f11c845713f2390741a9dd66a9e9657b88a
guypi001 <guy-parfait.aboi18@inphb.ci>
2021-08-18 19:10:24 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/example/app_lock/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_lock
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(MaterialApp(
+      home: MyApp(),
+    ));
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(title: Text('User Login Form')),
+            body: Center(child: LoginUser())));
+  }
+}
+
+class LoginUser extends StatefulWidget {
+  LoginUserState createState() => LoginUserState();
+}
+
+class LoginUserState extends State {
+  // For CircularProgressIndicator.
+  bool visible = false;
+
+  // Getting value from TextField widget.
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  Future userLogin() async {
+    // Showing CircularProgressIndicator.
+    setState(() {
+      visible = true;
+    });
+
+    // Getting value from Controller
+    String email = emailController.text;
+    String password = passwordController.text;
+
+    // SERVER LOGIN API URL
+    var url = Uri.parse('https://192.168.1.17/serrureconnect/login_user.php');
+
+    // Store all data with Param Name.
+    var data = {'email': email, 'password': password};
+
+    // Starting Web API Call.s
+    var response = await http.post(url, body: json.encode(data));
+
+    // Getting Server response into variable.
+    var message = jsonDecode(response.body);
+
+    // If the Response Message is Matched.
+    if (message == 'Login Matched') {
+      // Hiding the CircularProgressIndicator.
+      setState(() {
+        visible = false;
+      });
+
+      // Navigate to Profile Screen & Sending Email to Next Screen.
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  ProfileScreen(email: emailController.text)));
+    } else {
+      // If Email or Password did not Matched.
+      // Hiding the CircularProgressIndicator.
+      setState(() {
+        visible = false;
+      });
+
+      // Showing Alert Dialog with Response JSON Message.
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: new Text(message),
+            actions: <Widget>[
+              TextButton(
+                child: new Text("OK"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Center(
+      child: Column(
+        children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text('User Login Form', style: TextStyle(fontSize: 21))),
+          Divider(),
+          Container(
+              width: 280,
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                controller: emailController,
+                autocorrect: true,
+                decoration: InputDecoration(hintText: 'Enter Your Email Here'),
+              )),
+          Container(
+              width: 280,
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                controller: passwordController,
+                autocorrect: true,
+                obscureText: true,
+                decoration:
+                    InputDecoration(hintText: 'Enter Your Password Here'),
+              )),
+          ElevatedButton(
+            child: Text('Click Here To Login'),
+            onPressed: userLogin,
+          ),
+          Visibility(
+              visible: visible,
+              child: Container(
+                  margin: EdgeInsets.only(bottom: 30),
+                  child: CircularProgressIndicator())),
+        ],
+      ),
+    )));
+  }
+}
+
+class ProfileScreen extends StatelessWidget {
+//Creating String Var to Hold sent Email.
+  final String email;
+
+// Receiving Email using Constructor.
+  ProfileScreen({Key? key, required this.email}) : super(key: key);
+
+// User Logout Function.
+  logout(BuildContext context) {
+    Navigator.pop(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+                title: Text('Profile Screen'),
+                automaticallyImplyLeading: false),
+            body: Center(
+                child: Column(
+              children: <Widget>[
+                Container(
+                    width: 280,
+                    padding: EdgeInsets.all(10.0),
+                    child: Text('Email = ' + '\n' + email,
+                        style: TextStyle(fontSize: 20))),
+                ElevatedButton(
+                  child: Text('Button'),
+                  onPressed: () {
+                    logout(context);
+                  },
+                ),
+              ],
+            ))));
+  }
+}
